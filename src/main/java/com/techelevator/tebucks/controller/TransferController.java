95112ce17469ec9cc9b2279d2db5b2bea88abc52
Andie Fullmer <amfullmer@gmail.com>
2024-03-06 14:48:10 -0500

    controller finished

--- src/main/java/com/techelevator/tebucks/controller/TransferController.java
+++ src/main/java/com/techelevator/tebucks/controller/TransferController.java
@@ -4,14 +4,16 @@ import com.techelevator.tebucks.dao.AccountDao;
 import com.techelevator.tebucks.dao.JdbcAccountDao;
 import com.techelevator.tebucks.dao.JdbcTransferDao;
 import com.techelevator.tebucks.dao.TransferDao;
+import com.techelevator.tebucks.exception.DaoException;
 import com.techelevator.tebucks.model.Account;
 import com.techelevator.tebucks.model.Transfer;
 import com.techelevator.tebucks.security.dao.UserDao;
 import com.techelevator.tebucks.security.model.User;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
+import javax.validation.Valid;
 import java.security.Principal;
 import java.util.List;
 
@@ -42,12 +44,58 @@ public class TransferController {
         return transferDao.getAccountTransfers(userId);
     }
 
+    @GetMapping("/transfers/{id}")
+    public Transfer getTransferById (@PathVariable int id) {
+        Transfer transfer = transferDao.getTransferById(id);
+        if (transfer == null) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Transfer not found.");
+        } else {
+            return transfer;
+        }
+    }
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping("/transfers")
+    public Transfer createTransfer(@Valid @RequestBody Transfer transfer) {
+        return transferDao.createTransfer(transfer);
+    }
+
+    @PutMapping("/transfers/{id}/status")
+    public Transfer updateTransder(@Valid @RequestBody Transfer transferToUpdate,
+                                   @PathVariable int id) {
+        if (id != transferToUpdate.getTransferId() && transferToUpdate.getTransferId() != 0) {
+            throw new ResponseStatusException(HttpStatus.CONFLICT, "Reservation ids are in conflict.");
+        }
+
+        transferToUpdate.setTransferId(id);
+
+        try {
+            return transferDao.updateTransfer(transferToUpdate);
+        } catch (DaoException e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Transfer not found.");
+        }
+    }
+
     private User getLoggedInUserByPrincipal(Principal principal) {
         String username = principal.getName();
         return userDao.getUserByUsername(username);
     }
 
+    @GetMapping("/users")
+    public List<User> getUserList(Principal principal) {
+        User loggedInUser = getLoggedInUserByPrincipal(principal);
+        List<User> users = userDao.getUserList();
 
-    //TODO: get user list.
+        for (User user: users) {
+            if (user == loggedInUser) {
+                users.remove(user);
+            }
+        }
+        
+        return users;
+    }
+
+
+
 
 }
--- src/main/java/com/techelevator/tebucks/security/dao/UserDao.java
+++ src/main/java/com/techelevator/tebucks/security/dao/UserDao.java
@@ -3,7 +3,11 @@ package com.techelevator.tebucks.security.dao;
 import com.techelevator.tebucks.security.model.RegisterUserDto;
 import com.techelevator.tebucks.security.model.User;
 
+import java.util.List;
+
 public interface UserDao {
+
+    public List<User> getUserList();
 
     User getUserByUsername(String username);
 
