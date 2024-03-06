3d7e97c451c0180bcff6d39e247a04eb31c87dba
Lauren <laurenbanks15021@gmail.com>
2024-03-06 11:14:41 -0500

    Database complete

--- database/tebucks.sql
+++ database/tebucks.sql
@@ -1,18 +1,37 @@
 BEGIN TRANSACTION;
 
-DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS users, transfers, accounts;
 
 CREATE TABLE users (
-	user_id serial NOT NULL,
-	username varchar(50) NOT NULL,
-	password_hash varchar(200) NOT NULL,
-	first_name VARCHAR(50),
+    user_id serial NOT NULL,
+    username varchar(50) NOT NULL,
+    password_hash varchar(200) NOT NULL,
+    first_name VARCHAR(50),
     last_name VARCHAR(50),
     email VARCHAR(50),
-	role varchar(20),
-	CONSTRAINT pk_users PRIMARY KEY (user_id),
-	CONSTRAINT uq_username UNIQUE (username)
+    role varchar(20),
+    CONSTRAINT pk_users PRIMARY KEY (user_id),
+    CONSTRAINT uq_username UNIQUE (username)
 );
 
+CREATE TABLE transfers (
+    transfer_id serial PRIMARY KEY,
+    user_from_id int NOT NULL,
+    user_to_id int NOT NULL,
+    amount_to_transfer numeric(10,2) not null, 
+    is_successful boolean DEFAULT true, 
+    CONSTRAINT CHK_amount_gt_0 CHECK (amount_to_transfer > 0)
+);
+
+CREATE TABLE accounts (
+    account_id serial PRIMARY KEY,
+    user_id int NOT NULL,
+    balance numeric (10,2),
+    CONSTRAINT fk_users FOREIGN KEY (user_id) REFERENCES users (user_id),
+    CONSTRAINT CHK_balance_not_negative CHECK (balance >= 0)
+);
 
 COMMIT TRANSACTION;
+
+
+
