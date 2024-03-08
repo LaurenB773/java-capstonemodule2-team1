7cffe820ff3408819eb248f145cd7a35de35bf8d
Andie Fullmer <amfullmer@gmail.com>
2024-03-08 15:16:29 -0500

    tears service

---
+++ src/main/java/com/techelevator/tebucks/TEARS/TearsLogDto.java
@@ -0,0 +1,40 @@
+package com.techelevator.tebucks.TEARS;
+
+public class TearsLogDto {
+    private String description;
+    private String username_from;
+    private String username_to;
+    private double amount;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getUsername_from() {
+        return username_from;
+    }
+
+    public void setUsername_from(String username_from) {
+        this.username_from = username_from;
+    }
+
+    public String getUsername_to() {
+        return username_to;
+    }
+
+    public void setUsername_to(String username_to) {
+        this.username_to = username_to;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+}
--- src/main/java/com/techelevator/tebucks/TEARS/TearsService.java
+++ src/main/java/com/techelevator/tebucks/TEARS/TearsService.java
@@ -1,29 +1,60 @@
 package com.techelevator.tebucks.TEARS;
 
 import com.techelevator.tebucks.exception.DaoException;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
+import org.jboss.logging.BasicLogger;
+import org.springframework.http.*;
 import org.springframework.web.client.ResourceAccessException;
 import org.springframework.web.client.RestClientResponseException;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class TearsService {
     private static final String API_BASE_URL = "https://tears.azurewebsites.net/";
-    private final RestTemplate restTemplate = new RestTemplate();
+    private RestTemplate restTemplate = new RestTemplate();
 
-    public TearsLog addLog(TearsLog tearsLog) { //need to be DTO?
+    public TearsService(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
+    public String login(String userName, String password) {
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
-        HttpEntity<TearsLog> entity = new HttpEntity<>(tearsLog, headers);
 
-        TearsLog returnedTears = null;
+        Map<String, String> requestBody = new HashMap<>();
+        requestBody.put("andie", userName);
+        requestBody.put("me", password);
+
+        HttpEntity<Map<String, String>> entity = new HttpEntity<>(requestBody, headers);
+
         try {
-            returnedTears = restTemplate.postForObject(API_BASE_URL + "/api/TxLog", entity, TearsLog.class);
+            ResponseEntity<String> response = restTemplate.postForEntity(API_BASE_URL + "/login", entity, String.class);
+
+            if (response.getStatusCode() == HttpStatus.OK) {
+                return response.getBody();
+            } else {
+                throw new ResponseStatusException(HttpStatus.FORBIDDEN);
+            }
+        } catch (RestClientResponseException | ResourceAccessException e) {
+            throw new DaoException("Authentication failed", e);
+        }
+    }
+
+    public TearsLogDto addLog(TearsLogDto tearsLog, String authToken) { //need to be DTO?
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.set("Authorization", "Bearer" + authToken);
+        HttpEntity<TearsLogDto> entity = new HttpEntity<>(tearsLog, headers);
+
+        TearsLogDto returnedTears = null;
+        try {
+            returnedTears = restTemplate.postForObject(API_BASE_URL + "/api/TxLog", entity, TearsLogDto.class);
         } catch (RestClientResponseException | ResourceAccessException e) {
             throw new DaoException("Something went wrong.", e);
         }
         return returnedTears;
     }
-    
+
 }
