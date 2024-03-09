b0cfb580ee80551f8d206e7562b6d9b0861d43b1
Andie Fullmer <amfullmer@gmail.com>
2024-03-09 16:13:08 -0500

    account dao testing progress

--- src/test/java/com/techelevator/tebucks/dao/JdbcAccountDaoTests.java
+++ src/test/java/com/techelevator/tebucks/dao/JdbcAccountDaoTests.java
@@ -2,6 +2,7 @@ package com.techelevator.tebucks.dao;
 
 import com.techelevator.tebucks.security.dao.JdbcUserDao;
 import org.junit.Before;
+import org.junit.Test;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 public class JdbcAccountDaoTests extends BaseDaoTests {
@@ -15,4 +16,20 @@ public class JdbcAccountDaoTests extends BaseDaoTests {
         JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
         sut = new JdbcAccountDao(jdbcTemplate);
     }
+
+    @Test
+    public void getAccount_returns_correct_account_with_valid_id() {
+
+    }
+
+    @Test
+    public void getAccount_returns_null_with_invalid_id() {
+
+    }
+
+    @Test
+    public void updateBalance_exchanges_correct_amount() {
+
+    }
+    
 }
